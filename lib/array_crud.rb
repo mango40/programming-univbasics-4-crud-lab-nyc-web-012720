0567fe6e08669fa05e9b9acff297091bb87ce896
Manoj Patel <nycdays300@gmail.com>
2020-01-21 17:29:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ customers_new = ["a", "b", "c", "d"]
 end
 
 def add_element_to_end_of_array(array, element)
-customers_new.push ("f")
+customers_new.push 'f'
 end
 
 def add_element_to_start_of_array(array, element)
