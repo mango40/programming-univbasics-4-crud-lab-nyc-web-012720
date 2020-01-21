8f1b1a56e2eb4d1753371158367d80935e798212
Manoj Patel <nycdays300@gmail.com>
2020-01-21 17:13:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,11 +4,10 @@ end
 
 def create_an_array
 customers = [1, 2, 3, 4]
-
 end
 
 def add_element_to_end_of_array(array, element)
-
+customers << 5
 end
 
 def add_element_to_start_of_array(array, element)
